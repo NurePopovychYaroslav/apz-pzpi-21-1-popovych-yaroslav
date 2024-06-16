3b53dcc35ba331765f22debe70899197334d1ec2
yarikpo <yaroslavpopovich04@gmail.com>
2024-06-16 14:01:29 +0300

    Task2: added business logic with check for ability for student to access the door

---
+++ Task2/src/main/java/ua/clamor1s/eLock/controller/BusinessLoginController.java
@@ -0,0 +1,77 @@
+package ua.clamor1s.eLock.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.clamor1s.eLock.dto.response.AreaResponse;
+import ua.clamor1s.eLock.dto.response.CampusResponse;
+import ua.clamor1s.eLock.dto.response.DoorResponse;
+import ua.clamor1s.eLock.dto.response.StudentResponse;
+import ua.clamor1s.eLock.facade.AreaFacade;
+import ua.clamor1s.eLock.facade.CampusFacade;
+import ua.clamor1s.eLock.facade.DoorFacade;
+import ua.clamor1s.eLock.facade.StudentFacade;
+import ua.clamor1s.eLock.utils.annotations.Register;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/logic")
+public class BusinessLoginController {
+
+    private final StudentFacade studentFacade;
+    private final DoorFacade doorFacade;
+    private final AreaFacade areaFacade;
+    private final CampusFacade campusFacade;
+
+    @GetMapping
+    @Register
+    public String index(Model model) {
+        List<StudentResponse> students = studentFacade.getAllStudents();
+        List<CampusResponse> campuses = campusFacade.getAllByCurrentUser();
+
+        model.addAttribute("students", students);
+        model.addAttribute("campuses", campuses);
+        return "logic/index";
+    }
+
+    @GetMapping("/area")
+    public String getArea(@RequestParam("campus") Long campusId, Model model) {
+        if (campusId.equals(-1L)) {
+            return "fragments/logic/logicInputs :: emptyAreaDoorFormFragment";
+        }
+        List<AreaResponse> areas = areaFacade.getAllByCampusId(campusId);
+        model.addAttribute("areas", areas);
+        return "fragments/logic/logicInputs :: areaDoorFormFragment";
+    }
+
+    @GetMapping("/door")
+    public String getDoor(@RequestParam("area") Long areaId, Model model) {
+        if (areaId.equals(-1L)) {
+            return "fragments/logic/logicInputs :: emptyDoorFormFragment";
+        }
+        List<DoorResponse> doors = doorFacade.getFromDoors(areaId);
+        model.addAttribute("doors", doors);
+        return "fragments/logic/logicInputs :: doorFormFragment";
+    }
+
+    @GetMapping("/door-available")
+    public String getAvailable(@RequestParam("door") Long doorId,
+                               @RequestParam("student") Long studentId,
+                               @RequestParam("area") Long areaId,
+                               @RequestParam("campus") Long campusId,
+                               Model model) {
+        if (doorId.equals(-1L) || studentId.equals(-1L) || areaId.equals(-1L) || campusId.equals(-1L)) {
+            return "fragments/logic/logicInputs :: emptyResult";
+        }
+
+        boolean available = studentFacade.isDoorAvailable(doorId, studentId);
+        model.addAttribute("available", available);
+        return "fragments/logic/logicInputs :: resultPermission";
+    }
+}
